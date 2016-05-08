f32004a928164c50c68a875e9835a47e89538f6b
Bill Yu <han.yu@duke.edu>
2016-05-08 10:28:35 -0400

    fix filter bug and style errors

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 	function filter($content) {
 		$content = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $content);
-		$content = preg_replace('script', "", $content);
+		$content = preg_replace('/script/', "", $content);
 		return $content;
 	}
 ?>
