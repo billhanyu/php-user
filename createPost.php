278b4546e409810365f5f1988a6f21daa15e189c
Bill Yu <han.yu@duke.edu>
2016-05-09 20:47:05 -0400

    limit min length and cache input

--- createPost.php
+++ createPost.php
@@ -7,19 +7,39 @@
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $topic = $_POST['topic'];
+				$_SESSION['newPostTopic'] = $topic;
         $content = $_POST['content'];
+				$_SESSION['newPostContent'] = $content;
 				$content = filter($content);
 				$currentDate = date("M j, Y");
 				$author = $_SESSION['login_user'];
 
-				if (!$author){
-					header("location: /login.php?newPost=true");
+				$canPost = true;
+				
+				if (strlen($topic) < 1) {
+					$canPost = false;
+				?>
+					<script>alert("Topic is too short.");</script>
+				<?php	
+				}
+				
+				if ($canPost && strlen($content) < 20) {
+					$canPost = false;
+				?>
+					<script>alert("Content must be more than 20 characters.");</script>
+				<?php
 				}
 
-        $query = "INSERT INTO post (id, topic, content, author, post_time) VALUES (NULL, '$topic', '$content', '$author', '$currentDate')";
-        $success = mysqli_query($db, $query);
-				fwrite($handle, $success);
-        header("location: /welcome.php");
+				if (!$author){
+					header("location: /login.php?newPost=true");
+					$canPost = false;
+				}
+
+				if ($canPost) {
+        	$query = "INSERT INTO post (id, topic, content, author, post_time) VALUES (NULL, '$topic', '$content', '$author', '$currentDate')";
+        	$success = mysqli_query($db, $query);
+        	header("location: /welcome.php");
+				}
 	}
 ?>
 
@@ -37,9 +57,9 @@
 		<div class = "center">
 			<form action = "" method = "post">
       	<h2>Topic: </h2><br>
-      	<input type = "text" name = "topic" class = "topicBox"/><br /><br />
+      	<input type = "text" name = "topic" class = "topicBox" value = "<?php echo $_SESSION['newPostTopic'];?>"/><br /><br />
         <h2>Content: </h2><br>
-        <textarea name = "content" class = "contentBox" rows = "20"></textarea><br/><br />
+        <textarea name = "content" class = "contentBox" rows = "20"><?php echo $_SESSION['newPostContent'];?></textarea><br/><br />
         <input id = "postButton" type = "submit" value = " Post "/><br />
       </form>
 		</div>
